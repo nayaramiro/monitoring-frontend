7e7aecc1742708253fbd27504d3692daa74bbb2f
nayaramiro <nayaramiro.be@gmail.com>
2025-07-05 22:15:04 +0200

    Frontend initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [metrics, setMetrics] = useState([]);
+
+  useEffect(() => {
+    fetch("https://monitoring-backend-production-4e95.up.railway.app/api.php")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setMetrics(data.metrics);
+        } else {
+          console.error("Erreur API :", data.error);
+        }
+      })
+      .catch.error((err) => console.error("Fetch error:", err));
+  }, []);
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Dernières Métrics</div>
+      <ul>
+        {metrics.map((m, index) => (
+          <li key={index}>
+            {m.timestamp} : {m.value}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
